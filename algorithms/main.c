c5b04982f7654b2395fdcc589404a473cf113aff
Ninlgde <ninlgde.ma@gmail.com>
2015-08-18 17:50:03 +0800

    Initial Commit

---
+++ algorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithms
+//
+//  Created by MalikMa on 15/8/18.
+//  Copyright (c) 2015年 ninlgde. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
